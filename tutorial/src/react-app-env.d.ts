3d139cefda53ec102cef40b99952cb7361469d92
Masaya Sano <snmsy61@gmail.com>
2019-04-30 21:39:42 +0900

    fix

--- tutorial/src/react-app-env.d.ts
+++ tutorial/src/react-app-env.d.ts
@@ -1,1 +1,22 @@
 /// <reference types="react-scripts" />
+
+interface SquareProps {
+  value: any;
+  onClick: () => void;
+}
+
+type SquareTypea = 'O' | 'X' | null;
+
+interface BoardProps {
+  squares: SquareType[];
+  onClick: (i: number) => void;
+}
+
+interface HistoryData {
+  squares: SquareType[];
+}
+interface GameState {
+  history: HistoryData[];
+  xIsNext: boolean;
+  stepNumber: number;
+}
